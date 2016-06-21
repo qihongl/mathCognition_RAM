87867c3179a5ad229d9c22176f3a62f1776a137b
Qihong Lu <lvqihong1992@gmail.com>
2016-06-21 09:06:51 -0700

    convert img to tfr format

---
+++ src/toTFR/convert2tfr.py
@@ -0,0 +1,31 @@
+# ref: http://stackoverflow.com/questions/33849617/how-do-i-convert-a-directory-of-jpeg-images-to-tfrecords-file-in-tensorflow
+
+def _int64_feature(value):
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _bytes_feature(value):
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+# images and labels array as input
+def convert_to(images, labels, name):
+  num_examples = labels.shape[0]
+  if images.shape[0] != num_examples:
+    raise ValueError("Images size %d does not match label size %d." %
+                     (images.shape[0], num_examples))
+  rows = images.shape[1]
+  cols = images.shape[2]
+  depth = images.shape[3]
+
+  filename = os.path.join(FLAGS.directory, name + '.tfrecords')
+  print('Writing', filename)
+  writer = tf.python_io.TFRecordWriter(filename)
+  for index in range(num_examples):
+    image_raw = images[index].tostring()
+    example = tf.train.Example(features=tf.train.Features(feature={
+        'height': _int64_feature(rows),
+        'width': _int64_feature(cols),
+        'depth': _int64_feature(depth),
+        'label': _int64_feature(int(labels[index])),
+        'image_raw': _bytes_feature(image_raw)}))
+    writer.write(example.SerializeToString())
