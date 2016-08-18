dafdaf6080bba1282b2836cd9f3e84d235feb873
Qihong Lu <lvqihong1992@gmail.com>
2016-08-18 15:59:12 -0700

    testing file for the getData func

--- src/fixateOneObj/testGetData.py
+++ src/fixateOneObj/testGetData.py
@@ -3,38 +3,46 @@ import matplotlib as mpl
 import numpy as np
 from getData import getData
 
-showImage = 1
+pyplot.close("all")
 
+showImage = 1
 # datasetName = 'oneObj_big'
 datasetName = 'multiObj_balanced'
 hasLabel = True
 img_size1 = 90
 img_size2 = 90
 batch_size = 10
+maxNumObj = 7
 
-imgBatch, nextY, coords, _ = getData(batch_size, datasetName, img_size1, img_size2, hasLabel)
+selectedImgIdx = 0
+
+imgBatch, nextY, coords, _ = getData(batch_size, datasetName, img_size1, img_size2, hasLabel, maxNumObj)
 for i in xrange(batch_size):
     nextY[i] = int(nextY[i])
 
 print 'image batch dimension:'
 print np.shape(imgBatch)
 
-# print 'image indices:'
-# print img_idx
+
+thiscoord = coords[selectedImgIdx]
+print np.shape(coords)
+print np.shape(thiscoord)
+print thiscoord[:,0]
+print thiscoord[:,1]
+
+
+print nextY
+
+print np.zeros((batch_size, maxNumObj), dtype=bool)
+
+print np.zeros(batch_size)
 
 # show the 1st image
-selectedImgIdx = 1
+
 numObjs = nextY[selectedImgIdx]
 print 'number of objects = %d' % (numObjs)
 
-thiscoords = coords[selectedImgIdx]
-# thiscoords = np.array(thiscoords)
-print thiscoords
-print type(thiscoords)
-# print np.reshape(thiscoords, [2,numObjs])
-
 if showImage:
-
     # make a color map of fixed colors
     cmap = mpl.colors.ListedColormap(['black', 'white'])
     tempImg = np.reshape(imgBatch[selectedImgIdx,:], [img_size1, img_size2])
