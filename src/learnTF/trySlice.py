b24bd4d19065e075ccdb131b3c6423a6dee84d66
Qihong Lu <lvqihong1992@gmail.com>
2016-08-22 13:11:02 -0700

    testing

--- src/learnTF/trySlice.py
+++ src/learnTF/trySlice.py
@@ -1,27 +1,34 @@
 import numpy as np
 import tensorflow as tf
 
-batchSize = 5
-imgSize = 3
+# batchSize = 10
+# imgSize = 3
+# n_glimpses = 7
+#
+# data = np.reshape(np.arange(n_glimpses * batchSize),[n_glimpses,batchSize,1])
+# imgs = tf.Variable(data, name = 'images')
+# imgs = tf.cast(imgs, tf.int32)
+#
+#
+# slice = tf.slice(imgs, [n_glimpses-1,0,0], [1,10,1])
+#
+# with tf.Session() as session:
+#     session.run(tf.initialize_all_variables())
+#     print(session.run(imgs))
+#     print(session.run(slice))
 
-data = np.reshape(np.arange(batchSize * (imgSize**2)),[batchSize,imgSize,imgSize])
 
-coords = np.zeros((batchSize,2))
-print coords
 
-imgs = tf.Variable(data, name = 'images')
-imgs = tf.cast(imgs, tf.int32)
-coords = tf.Variable(coords, name = 'pixelCoordinates')
+numObjs = 5
 
-# kthCoord = tf.slice(imgs, [0,2,0], [batchSize,1,2])
-# kthCoord = tf.reduce_sum(kthCoord, 1)
+data = np.arange(numObjs)
+var = tf.Variable(data, name = 'images')
+var = tf.cast(var, tf.int32)
 
-slice = tf.slice(imgs, [0,0,0], [5,1,1])
+
+slice = tf.slice(var, [0], [3])
 
 with tf.Session() as session:
     session.run(tf.initialize_all_variables())
-    print(session.run(imgs))
+    print(session.run(var))
     print(session.run(slice))
-
-
-
