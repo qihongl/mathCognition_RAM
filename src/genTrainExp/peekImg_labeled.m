6d4b4a0e84f9c8afecf50fb1577b23b8f75002cb
Qihong Lu <lvqihong1992@gmail.com>
2016-08-22 13:11:12 -0700

    peek the image generated

--- src/genTrainExp/peekImg_labeled.m
+++ src/genTrainExp/peekImg_labeled.m
@@ -1,12 +1,12 @@
-load ../../datasets/multiObj_balanced/multiObj_balanced001.mat
+load ../../datasets/oneObj_centered/oneObj_centered002.mat
 % vectorImg
 label = vectorImg(1);
-img_size = 90;
+img_size = 28;
 
 figure(2)
 imagesc(reshape(vectorImg(1:img_size*img_size),img_size,img_size))
 colorbar
 
 numObj = vectorImg(img_size*img_size+1)
-coords = reshape(vectorImg(img_size*img_size+2:end), [numObj,2])
+coords = reshape(vectorImg(img_size*img_size+2:end), [7,2])
 
