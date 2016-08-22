4b1a57c136b461f9a23ffc53b58f8bf92c0775f8
Qihong Lu <lvqihong1992@gmail.com>
2016-08-22 09:51:04 -0700

    add a test condition - fixed num obj

--- src/genTrainExp/outputImgs.m
+++ src/genTrainExp/outputImgs.m
@@ -4,12 +4,12 @@
 clear variables; clf; 
 
 % high level parameters
-numImages = 1000;              % the number of output images 
+numImages = 700;              % the number of output images 
 
 param.showImg = 0;          % display a sample image 
 param.saveImg = 0;          % same the image 
 param.saveStruct = 1;       % save the matrix representation 
-imgName = 'multiObj_balanced';
+imgName = 'fourObj_realistic';
 % imgName = 'oneObj';
 % imgName = 'oneObj_big';
 param.saveDir = strcat('../../datasets/', imgName);
@@ -22,16 +22,16 @@ allPatterns = {'prototype', 'randomVec', 'allPoss'};
 param.pattern = allPatterns{2};
 
 alignmentPatterns = {'left', 'center'};
-param.alignment = alignmentPatterns{1};
+param.alignment = alignmentPatterns{2};
 
-param.max_obj_num = 7; 
-param.obj_radius = 3;       % size of the object 
+param.max_obj_num = 4; 
+param.obj_radius = 4;       % size of the object 
 param.varySize = 0;         % random radius for object 
 
-param.frame_ver = 90;       % the length of the image
-param.frame_hor = 90;       % the height of the image
-param.frame_boundary = param.obj_radius * 3;    % space to the boundary of img
-param.frame_space = param.obj_radius * 4;       % space in between objects
+param.frame_ver = 60;       % the length of the image
+param.frame_hor = 60;       % the height of the image
+param.frame_boundary = param.obj_radius * 2;    % space to the boundary of img
+param.frame_space = param.obj_radius * 3.5;       % space in between objects
 
 param.distortion_x = param.obj_radius * 1; % magnitude of distortion
 param.distortion_y = param.obj_radius * 1;
@@ -40,8 +40,9 @@ param.frame_randVecDistribution = noise_allDistributions{2};
 
 param.supervised = true; 
 
-numObj_allDistributions = {'uniform', 'realistic'};
-param.numObj_sampling = 'uniform';
+numObj_allDistributions = {'uniform', 'realistic', 'fixed'};
+param.numObj_sampling = numObj_allDistributions{2};
+% param.staticNumObj = 4; 
 
 %% generate images 
 checkParameters(param)
@@ -51,6 +52,8 @@ for n = 1 : numImages
         param.obj_num = generateNum(param.max_obj_num);
     elseif strcmp(param.numObj_sampling, 'uniform')
         param.obj_num = randsample(param.max_obj_num,1,true);
+    elseif strcmp(param.numObj_sampling, 'fixed')
+        param.obj_num = param.staticNumObj; 
     end
     % param.obj_num = 5;          % number of objects
     
